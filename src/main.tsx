508bd55eda90c61c0fd931f688633f5609fcdf66
Nolunga <nolungan@gmail.com>
2023-05-18 10:39:03 +0200

    feat(app provider): wrap app provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import AppProvider from './context/App'
 import './main.css'
 import Navigation from './navigation'
 import theme from './theme'
@@ -10,8 +11,10 @@ const root = createRoot(container!)
 
 root.render(
   <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <Navigation />
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    </AppProvider>
   </ChakraProvider>
 )
